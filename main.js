08ab043424e44dc2d7c8b26d9f00d0d6de9ea0ea
kentodaikoku <kedks090597@gmail.com>
2023-02-27 14:57:20 +0900

    history_1

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+// 画像フェード表示
 window.onload = () => {
   let profileImg = document.getElementById('fadein');
   profileImg.style.opacity = '1';
-  profileImg.style.transform = 'translateY(0)';
+  profileImg.style.transform = 'translateX(0)';
 };
