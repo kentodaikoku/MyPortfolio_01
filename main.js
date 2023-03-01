ac2639472f65187159895f77d98933cb8866b1d3
kentodaikoku <kedks090597@gmail.com>
2023-03-01 21:12:43 +0900

    javascript_2

--- main.js
+++ main.js
@@ -3,4 +3,27 @@ window.onload = () => {
   let profileImg = document.getElementById('fadein');
   profileImg.style.opacity = '1';
   profileImg.style.transform = 'translateX(0)';
+
+  let profileName = document.querySelector('.profile-name');
+  // console.log(profileName);
+  profileName.classList.toggle('onload');
 };
+
+window.setTimeout(() => {
+  let profileName = document.querySelector('.profile-name');
+  profileName.classList.toggle('onload');
+}, 2000);
+
+
+// 画像スクロール表示
+
+
+// ハンバーガーメニュー
+
+
+
+// 英語版表示？
+
+
+
+// ナイトモード処理？
