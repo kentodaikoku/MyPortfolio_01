14e0b5775b1eb6d24addae6352362eb6a65c351d
kentodaikoku <kedks090597@gmail.com>
2023-09-03 14:10:11 +0800

    history contetns --> json

--- js/getJson.js
+++ js/getJson.js
@@ -1,8 +1,11 @@
-let url = "../json/myskill.json";
+const skillUrl = "../json/mySkill.json";
+const historyUrl = "../json/myHistory.json";
+
 // let url = "http://kentodaikoku.html.xdomain.jp/portfolio/json/myskill.json";
 
 window.addEventListener('load', ()=> {
-  fetch(url).then(response => response.json()).then(data => getSkillJson(data));
+  fetch(skillUrl).then(response => response.json()).then(data => getSkillJson(data));
+  fetch(historyUrl).then(response => response.json()).then(data => getHistoryJson(data));
 })
 
 function getSkillJson(data) {
@@ -17,6 +20,18 @@ function getSkillJson(data) {
       contents += '</div>';
       contents += '<div class="skill-line ' + e.class + '"></div>';
     contents += '</div>';  
+  });
+
+  html.innerHTML = contents;
+}
+
+function getHistoryJson(data) {
+  let html = document.querySelector('.history-contents');
+  let contents = '';
+
+  data.forEach(e => {
+      contents += '<dt>' + e.date + '</dt>';
+      contents += '<dd>' + e.text + '</dd>';
   });
 
   html.innerHTML = contents;
